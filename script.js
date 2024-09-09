c5bf63e129bf0b54e921ff9c610f245ccd301b48
GroupGarcia <groupgarciabd@gmail.com>
2024-09-08 18:30:47 -0600

    FINAL
    YA QUEDO

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const navResponsive = document.querySelector('.nav-responsive');
-    const menuMovil = document.querySelector('#menu-movil');
-
-    navResponsive.addEventListener('click', function() {
-        menuMovil.classList.toggle('active');
-    });
-});
